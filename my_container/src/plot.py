11568d22ec4c6413bbad8a561e11384260297a37
ch-vassilissa <82966942+ch-vassilissa@users.noreply.github.com>
2022-03-03 22:15:53 +0600

    add path

--- my_container/src/plot.py
+++ my_container/src/plot.py
@@ -26,7 +26,8 @@ distribution_n2 = np.random.normal(mean,deviation,1000)*2
 
 sns_plot  = sns.distplot(distribution_n1, kde=True, rug=False)
 sns_plot  = sns.distplot(distribution_n2, kde=True, rug=False)
-plt.savefig('C:/Users/vasil/LearningFolder1/PycharmProjects/7_module/src/output/plot.png')
+#plt.savefig('C:/Users/vasil/LearningFolder1/PycharmProjects/7_module/src/output/plot.png') # для windows
+plt.savefig('./output/plot.png') # для ubuntu
 
 print('Файл успешно сохранен')
 
